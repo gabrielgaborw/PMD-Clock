77bce4644dfb1b597d89b97a1169ace6904cb0d3
Gabriel Gabor <gabrielgabor98@gmail.com>
2023-07-21 11:37:58 +0300

    Implemented button functionality

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,28 +2,51 @@ import React, { useEffect, useState } from 'react'
 
 const Timer = () => {
 	const [time, setTime] = useState<number>(1500)
+	const [started, setStarted] = useState<boolean>(false)
 
+	// Making the time pass if started is true
 	useEffect(() => {
-		const interval = setInterval(() => {
-			setTime((prevTime) => prevTime - 1)
-		}, 1000)
+		if(started) {
+			const interval = setInterval(() => {
+				setTime((prevTime) => prevTime - 1)
+			}, 1000)
 
-		return () => clearInterval(interval)
-	}, [])
+			return () => clearInterval(interval)
+		}
+	}, [started])
 
+	// Formatting time from a number
 	const formatTime = (time: number): string => {
 		const minutes = Math.floor(time / 60)
 		const seconds = time % 60
 		return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
 	}
 
+	// Buttons functionality
+	const handleStart = () => {
+		setStarted(true)
+	}
+	const handleStop = () => {
+		setStarted(false)
+	}
+	const handleReset = () => {
+		setTime(1500)
+		setStarted(false)
+	}
+
   return (
     <div>
       <h1>🍅</h1>
 			<p>{formatTime(time)}</p>
-			<button>Start</button>
-			<button>Stop</button>
-			<button>Reset</button>
+			<button onClick={handleStart}>
+				Start
+			</button>
+			<button onClick={handleStop}>
+				Stop
+			</button>
+			<button onClick={handleReset}>
+				Reset
+			</button>
     </div>
   )
 }
